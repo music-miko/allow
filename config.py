d478195c6b92bb3ad3da0c422c4a1c33957d117b
music-miko <voilet856@gmail.com>
2025-01-20 13:46:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7765021066:AAG29mmbKFKFeMn3pIFPB1SygH2M8i8sTL8")
+API_ID = int(os.environ.get("API_ID", "24620300"))
+API_HASH = os.environ.get("API_HASH", "9a098f01aa56c836f2e34aee4b7ef963")
 SPOILER_MODE = bool(os.environ.get("SPOILER_MODE", True))
