b09bf099ff0d8617e5e32c2dfddadcc162aca9a6
music-miko <voilet856@gmail.com>
2025-01-20 13:38:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 import re
 import config 
 
-url = "https://api.safone.me/nsfw"
 SPOILER = config.SPOILER_MODE
 slangf = 'slang_words.txt'
 with open(slangf, 'r') as f:
@@ -16,47 +15,9 @@ Bot = Client(
     api_hash=config.API_HASH
 )
 
-#-----------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
-@Bot.on_message(filters.private & filters.command("start"))
-async def start(bot, update):
-    await update.reply("""Hi there! I'm the Telegram Group Guardian bot. I'm here to help you keep your group clean and safe for everyone. Here are the main features I offer:
-
-• **Word Slagging:** I can detect and remove inappropriate language messages in your group. 
-
-• **Image Filtering:** I can also automatically detect and remove pornographic or NSFW images in your group. 
-
-To get started, simply add me to your Telegram group and promote me to admin 
-
-Thanks for using Telegram Group Guardian! Let's keep your group safe and respectful. Powered by @NACBOTS""")
-
-#-----------------------------------------------------------------
-
-@Bot.on_message(filters.group & filters.photo)
-async def image(bot, message):
-    sender = await Bot.get_chat_member(message.chat.id, message.from_user.id)
-    isadmin = sender.privileges
-    if not isadmin:
-        x = await message.download()
-        files = {"image": open(x, "rb")}
-        roi = requests.post(url, files=files)
-        data = roi.json()
-        nsfw = data["data"]["is_nsfw"]
-        porn = data["data"]["porn"]
-        if nsfw:
-            name = message.from_user.first_name
-            await message.delete()
-            if SPOILER:
-                await message.reply_photo(x, caption=f"""**WARNING ⚠️** (nude photo)
-
- **{name}** sent a nude photo
-
-{porn}% porn""", has_spoiler = True)
-
-
-#-----------------------------------------------------------------
-
-@Bot.on_message(filters.group & filters.text)
+@Bot.on_message(filters.group & filters.text & filters.command("guard"))
 async def slang(bot, message):
     sender = await Bot.get_chat_member(message.chat.id, message.from_user.id)
     isadmin = sender.privileges
